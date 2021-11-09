68f3cf6ea998a1da8601daac1e4bee55615dcdb3
Abd UlHameed Maree <abd.maree@scandinaviatech.com>
2021-11-09 15:45:49 +0200

    initial commit

---
+++ apps/scndainavia-tracking-dashboard/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'scndainavia-tracking-dashboard',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/scndainavia-tracking-dashboard',
+};
